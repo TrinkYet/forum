4805396b78e810f89c859c932b69d2d3480a3212
coldcutter <guhanya1@gmail.com>
2014-10-28 19:14:52 +0800

    cmt1

--- src/main/java/orz/wizard/mao/forum/service/GroupService.java
+++ src/main/java/orz/wizard/mao/forum/service/GroupService.java
@@ -1,10 +1,12 @@
 package orz.wizard.mao.forum.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import orz.wizard.mao.forum.dao.GroupDao;
 import orz.wizard.mao.forum.entity.Group;
 
+@Service
 public class GroupService {
     @Autowired
     private GroupDao groupDao;
--- src/main/java/orz/wizard/mao/forum/service/TopicService.java
+++ src/main/java/orz/wizard/mao/forum/service/TopicService.java
@@ -3,10 +3,12 @@ package orz.wizard.mao.forum.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import orz.wizard.mao.forum.dao.TopicDao;
 import orz.wizard.mao.forum.entity.Topic;
 
+@Service
 public class TopicService {
     @Autowired
     private TopicDao topicDao;
